59bcc52b2b1760527030077b25261be08082e66d
jran <55335329+jran14@users.noreply.github.com>
2019-10-18 21:21:24 -0500

    fifth

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,7 @@ import csv
 import statistics
 pl = []
 delta = []
+mon=[]
 #to read the csv file
 csvpath=os.path.join('..','budget_data.csv')
 
@@ -14,8 +15,10 @@ with open(csvpath, newline='') as csvfile:
     for row in csvreader:
         x = int(row[1])
         pl.append(x)
+        mon.append(row[0])
         months= months + 1
       #  print(pl)
+
         
 
 
@@ -32,16 +35,20 @@ print('Total Profit:',sum(pl))
 #average change in P/L over time
 for i in range(months-1):
     delta.append(pl[i+1]-pl[i])
-    m = statistics.mean(delta)
+m = statistics.mean(delta)
 print('Average Change: ${:.2f}'.format(m))
 #print('Average Change:', '$', round(statistics.mean(delta)))
 #print('Average Change:', '10.2f' % statistics.mean(delta))
 
 #greatest increase in profits
-for rows in csvreader:
-    if delta == max(delta)
-    print(row[0])
-#print('Greatest Increase:', max(delta))
+#for rows in csvreader:
+   # if delta == max(delta)
+   # print(row[0])
+max1= delta.index(max(delta))
+#print(max1)
+print('Greatest Increase in Profits:', mon[max1+1], '(',max(delta),')')
 #greatest decrease in losses 
-#print('Greatest Decrease:', min(delta))
+min1=delta.index(min(delta))
+#print(min1)
+print('Greatest Decrease in Profits:',  mon[min1+1], '(',min(delta),')')
 
